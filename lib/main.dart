709bed868fea8e1a0dfa3285fa73d4ac095bb3d2
Manerial <avr1l.dunmare@gmail.com>
2026-05-27 00:43:40 +0200

    Fix path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'app.dart';
+import 'package:ludo_pay_app/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
